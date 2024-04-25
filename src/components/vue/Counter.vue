32dd954c255c8b8ca192d86a9080d85f36ffc75d
kdmaniosy <kdmaniosy@ufpso.edu.co>
2024-04-25 13:01:35 -0500

    vueIntegration

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+    let count = ref(0);
+
+    function addValue(){
+        count.value ++;
+        console.log(count)
+    }
+</script>
+
+<template>
+    <div @click="addValue"  class="bg-blue-500 text-white py-2 px-4 rounded inline-block hover:cursor-pointer hover:bg-blue-600 transition-colors">
+        ¡Presioname! {{ count }}
+    </div>
+
+</template>
